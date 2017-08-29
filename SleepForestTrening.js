4556243b210ce9d4fcd436bf76b737a613a5f2e6
kamilvlcek <kamil@biomed.cas.cz>
2017-08-29 16:32:06 +0200

    SleepForest - nova sekvence ctvercu v treningu od Aleny
    - kratsi sekvence v jedne dvojici ctvercu - jen jednou kazde zvire
    - celkova sekvence dvojic ctvercu je 2x delsi - 32 opakovani

--- SleepForestTrening.js
+++ SleepForestTrening.js
@@ -1,18 +1,21 @@
 // TRENING - nastaveni sekvence dvoji ctvercu
 var SquarePairs=Array();
 // poradi dvojic ctvercu podle fazi treningu - tohle pole urcuje delku treningu
-SquarePairs=[['A','B'], ['F','C'], ['E','D'], ['I','H'], ['D','G'], ['B','C'], ['H','G'], ['I','F'],
-             ['C','B'], ['G','H'], ['C','F'], ['G','D'], ['F','I'], ['B','A'], ['D','E'], ['H','I']];
-// nova sekvence 9.3.2017 - ctverce v ruzne poradi (AB i BA), neopakuji se za sebou
+SquarePairs=[['A','B'], ['F','C'], ['E','D'], ['I','H'], ['D','G'], ['B','C'], ['H','G'], ['E','F'],
+             ['C','B'], ['D','E'], ['B','A'], ['G','H'], ['F','E'], ['H','I'], ['G','D'], ['C','F'], 
+             ['I','H'], ['B','C'], ['E','D'], ['H','G'], ['F','C'], ['A','B'], ['D','G'], ['E','F'],
+             ['C','B'], ['G','H'], ['F','E'], ['G','D'], ['H','I'], ['C','F'], ['D','E'], ['B','A']];
+  
+// nova sekvence 29.8.2017 -Alena - ctverce v ruznem poradi (AB i BA), neopakuji se za sebou, kratky pocet ukolu
 
 // jednoducha sekvence na projiti vsech ctvercu             
-//SquarePairs=[['A','B'],['B','C'],['C','F'],['F','I'],['I','H'],['H','G'],['G','D'],['D','E']];                
+//SquarePairs=[['A','B'],['B','C'],['C','F'],['F','E'],['E','D'],['D','G'],['G','H'],['H','I']];                
   
 
 // TRENING nastaveni sekvence zvirat v kazde dvojici ctvercu
 var AnimalSequence=Array();   // poradi zvirat podle fazi treningu
 // porad jedna sekvence
-AnimalSequence=[[0,1,0,1,10,11,10,11,0,10,1,11]]; // n<10 - prvni ctverec v poradi, n>10 druhy ctverec v poradi
+AnimalSequence=[[0,10,1,11]]; // n<10 - prvni ctverec v poradi, n>10 druhy ctverec v poradi
 
 // kratka sekvence na testovani skriptu
 //AnimalSequence=[/*debug*/[0,1,0,1,10,11,10,11]]; // n<10 - prvni ctverec v poradi, n>10 druhy ctverec v poradi
