dae14ac7deca35868ad0dcfb0532e365fb1fa231
kamilvlcek <kamil@biomed.cas.cz>
2017-12-07 17:28:46 +0100

    SleepForest - SleepForestPreTrening.js - prvni funkcni verze
    - clovek jde do mista A, B C a zpet do A
    - pise se jen nazev mista a na konci Konec

--- SleepForestPreTrening.js
+++ SleepForestPreTrening.js
@@ -1,7 +1,8 @@
 function init() {	
-	experiment.setMap("TEST-tisove_bludiste");
+	experiment.setMap("TEST-SleepForest-pretraining2-Alena");
 }
-var Hvezdy = ['hvezda1','hvezda2','hvezda3','hvezda4','hvezda5','hvezda6','hvezda7']; 
+var Hvezdy = ['AimAreaA','AimAreaB','AimAreaC','AimAreaA'];  // jmena cilovych oblasti
+var Cile = ['A','B','C','A'];     // pojmenovani cilu 
 var cislo = 0;
 var zpatky = 0;
 var castam = 0;
@@ -14,16 +15,18 @@ function run() {
 		experiment.setTrackRate(0.05);
 		experiment.setPlayerSpeed(1500);
 		//platform.get("plosina").doRotateTime(10000,9,-1);
-    text.create(1, 250, 50, 255, 255,0, 3,"STAR 1");
-    experiment.setPlayerRotation(0,33); // musim ho otocit vertikalne na nulu, protoze se bude pohybovat jen pomoci left right klaves
+    text.create(1, 250, 50, 255, 255,0, 3,"CIL A");
+    //experiment.setPlayerRotation(0,33); // musim ho otocit vertikalne na nulu, protoze se bude pohybovat jen pomoci left right klaves
+    preference.get(Hvezdy[cislo]).setVisible(false);
+    preference.get(Hvezdy[cislo]).setActive(true);   // CIL A se aktivuje 
 	  
 	}
-	if(preference.get("start").entered()){
-	  cas = parseFloat(preference.get("start").getPrevEnterTimeDiff());
-	  caszpet += cas;
-	  debug.log("cas zpet: "+caszpet+" s");
-		experiment.setStop();
-	}
+// 	if(preference.get("start").entered()){
+// 	  cas = parseFloat(preference.get("start").getPrevEnterTimeDiff());
+// 	  caszpet += cas;
+// 	  debug.log("cas zpet: "+caszpet+" s");
+// 		experiment.setStop();
+// 	}
 	if (key.pressed("o")){
 		preference.get(Hvezdy[cislo]).setVisible(true);
 	}
@@ -32,8 +35,9 @@ function run() {
 	}
 	
 	if(cislo >= 0 &&  preference.get(Hvezdy[cislo]).entered()){
-			cas = parseFloat(preference.get(Hvezdy[cislo]).getPrevEnterTimeDiff());
-			debug.log(Hvezdy[cislo]+": "+cas+" s");
+			cas = parseFloat(preference.get(Hvezdy[cislo]).getPrevEnterTimeDiff());   // bude to tady fungovat?
+			debug.log(Hvezdy[cislo]+": "+cas+" s");  // zapis do logu
+      debug.log('vstup do ' + (Hvezdy[cislo]));
 			if(zpatky) caszpet += cas; else castam += cas;
 			
 			//debug.log("vzdálenost: " + preference.get(Hvezdy[cislo]).getPrevEnterDistDiff());
@@ -46,18 +50,13 @@ function run() {
     	
     	if(cislo>=Hvezdy.length){
     	  debug.log("cas tam: "+castam+" s\n");
-				zpatky = 1; 
-				cislo -= 2; // z 8 chci udelat 6
-				text.modify(1,"STAR "+(cislo+1));
-				aktivni = Hvezdy[cislo];
-			} else if(cislo<0) {
-				text.modify(1,"Start"); 
-				aktivni = "start";
+        text.modify(1,"KONEC");
+        experiment.setStop();						 
 			} else {
-				text.modify(1,"STAR "+(cislo+1));
+				text.modify(1,"CIL "+Cile[cislo]);
 				aktivni = Hvezdy[cislo];
 			}
-			preference.get(aktivni).setVisible(true);
+			preference.get(aktivni).setVisible(false);
     	preference.get(aktivni).setActive(true);
   } 
 }
